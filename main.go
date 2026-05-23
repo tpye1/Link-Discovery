a2aa4769b0ccf5165e2d55d768619921e90a5e93
Tom Pye <tomspye1@gmail.com>
2026-05-23 14:48:26 +0100

    Deleted comment that I might not ever use

--- main.go
+++ main.go
@@ -95,14 +95,7 @@ func get_link_data(connection *connect_struct) link_data {
 	var cmd *exec.Cmd
 	if runtime.GOOS == "windows" {
 		device_argument = "[windows], " + "{" + (*(*connection).iface).Name + "}"
-		/*
-		cmd = exec.Command(
-			"whoami",
-			"C:\Users\tom\Desktop\ldlinux\helper\helper.exe",
-			device_argument
-		)
-		*/
-	} else {
+		} else {
 		device_argument = (*(*connection).iface).Name
 		
 		home := os.Getenv("HOME")
